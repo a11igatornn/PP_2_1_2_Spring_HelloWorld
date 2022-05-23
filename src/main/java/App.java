410007404e7e315d8c8f4ca930c9665f46544acc
a11igatornn <cgs-nn@mail.ru>
2022-05-23 17:04:30 +0300

    commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,21 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanHW1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(beanHW1.getMessage());
+        HelloWorld beanHW2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHW2.getMessage());
+
+        Cat beanC1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanC1.getName());
+        Cat beanC2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanC2.getName());
+
+        System.out.println(beanHW1 == beanHW2);
+        System.out.println(beanC1 == beanC2);
     }
 }
